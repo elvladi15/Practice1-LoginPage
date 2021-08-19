778f792f924c2a772a886206b2df52b1575140f1
Vladimir <vladimirfranciscodiaz@gmail.com>
2021-08-19 00:17:00 -0400

    Add project files.

---
+++ Practice1/Practice1/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practice1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+    
+        }
+
+        async private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(username.Text) || String.IsNullOrWhiteSpace(password.Text))
+            {
+                await DisplayAlert("Error", "Please fill all the fields", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Welcome", $"Hello, {username.Text}", "OK");
+            }
+        }
+    }
+}
